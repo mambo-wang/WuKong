7f2e65e95a8139ff8c0c76e28ed726b218d0958f
mambo-wang <mambo1991@163.com>
2020-03-08 21:25:58 +0800

    WuKong--完成邮件集成和banner

--- wukong-provider/src/main/java/com/wukong/provider/controller/TestController.java
+++ wukong-provider/src/main/java/com/wukong/provider/controller/TestController.java
@@ -2,10 +2,12 @@ package com.wukong.provider.controller;
 
 import com.wukong.common.model.BaseResult;
 import com.wukong.provider.config.rest.RestConnection;
+import com.wukong.provider.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +19,24 @@ public class TestController {
     @Autowired
     private RestConnection restConnection;
 
+    @Autowired
+    private MailService mailService;
+
     @GetMapping
     public BaseResult hello(){
 
         ResponseEntity<BaseResult> resultResponseEntity =  restConnection.get("/hello", ParameterizedTypeReference.forType(BaseResult.class));
 
         return resultResponseEntity.getBody();
+    }
+
+    /**
+     * sendAttachmentsMail
+     */
+    @PostMapping
+    public  BaseResult mail(){
+
+        mailService.sendSimpleMail("mambo1991@163.com", "测试", "测试事实上司是");
+        return BaseResult.success(null);
     }
 }
---
+++ wukong-provider/src/main/java/com/wukong/provider/service/MailService.java
@@ -0,0 +1,14 @@
+package com.wukong.provider.service;
+
+
+public interface MailService {
+
+    void sendSimpleMail(String to, String subject, String content);
+
+    void sendHtmlMail(String to, String subject, String content);
+
+    void sendAttachmentsMail(String to, String subject, String content, String filePath);
+
+    void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId);
+
+}
