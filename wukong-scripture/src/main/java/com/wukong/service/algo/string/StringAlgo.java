b31e9c787ebc2c5312426963bdbd8f2e431bfbaa
mambo-wang <mambo1991@163.com>
2020-03-23 18:09:33 +0800

    WuKong--字符串算法-反转字符串

--- wukong-scripture/src/main/java/com/wukong/service/algo/string/StringAlgo.java
+++ wukong-scripture/src/main/java/com/wukong/service/algo/string/StringAlgo.java
@@ -3,6 +3,25 @@ package com.wukong.service.algo.string;
 public class StringAlgo {
 
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringAlgo stringAlgo = new StringAlgo();
+
+        char[] str = "abcde".toCharArray();
+        stringAlgo.reverseString(str);
+        System.out.println(String.valueOf(str));
+    }
+
+    /**
+     * 344. 反转字符串
+     * @param s
+     */
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length; i++) {
+            if (i >= s.length - i - 1) {
+                break;
+            }
+            char temp = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = temp;
+        }
     }
 }
