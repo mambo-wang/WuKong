4281ce6198229c9d8eba155bb93581064146e67b
mambo-wang <mambo1991@163.com>
2020-03-28 11:00:08 +0800

    WuKong--leetCode每日一题 820 单词的压缩编码 Trie树

--- wukong-scripture/src/main/java/com/wukong/service/algo/daily/Daily202003.java
+++ wukong-scripture/src/main/java/com/wukong/service/algo/daily/Daily202003.java
@@ -36,6 +36,66 @@ public class Daily202003 {
     }
 
     /**
+     * 3月28日   820. 单词的压缩编码
+     * 题目：
+     * 给定一个单词列表，我们将这个列表编码成一个索引字符串 S 与一个索引列表 A。
+     * 例如，如果这个列表是 ["time", "me", "bell"]，我们就可以将其表示为 S = "time#bell#" 和 indexes = [0, 2, 5]。
+     * 对于每一个索引，我们可以通过从字符串 S 中索引的位置开始读取字符串，直到 "#" 结束，来恢复我们之前的单词列表。
+     * 那么成功对给定单词列表进行编码的最小字符串长度是多少呢？
+     * 链接：https://leetcode-cn.com/problems/short-encoding-of-words/solution/99-java-trie-tu-xie-gong-lue-bao-jiao-bao-hui-by-s/
+     * 解题思路：
+     * 为什么这题我们要用字典树做呢？因为我们需要知道单词列表里，哪些单词是其它某个单词的后缀。既然要求的是后缀，我们只要把单词的倒序插入字典树，再用字典树判断某个单词的逆序是否出现在字典树里就可以了。
+     * 我们必须要先插入单词长的数组，否则会有问题。比如如果我先插入了"em"，再插入"emit",会发现两个都可以插入进去，很显然是不对的，
+     * 所以在插入之前需要先根据单词的长度由长到短排序。
+     * @param words
+     * @return
+     */
+    public int minimumLengthEncoding(String[] words) {
+
+        int len = 0;
+        Trie trie = new Trie();
+        // 先对单词列表根据单词长度由长到短排序
+        Arrays.sort(words, (s1, s2) -> s2.length() - s1.length());
+        // 单词插入trie，返回该单词增加的编码长度
+        for (String word: words) {
+            len += trie.insert(word);
+        }
+        return len;
+    }
+
+    // 定义tire
+    class Trie {
+
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public int insert(String word) {
+            TrieNode cur = root;
+            boolean isNew = false;
+            // 倒着插入单词
+            for (int i = word.length() - 1; i >= 0; i--) {
+                int c = word.charAt(i) - 'a';
+                if (cur.children[c] == null) {
+                    isNew = true; // 是新单词
+                    cur.children[c] = new TrieNode();
+                }
+                cur = cur.children[c];
+            }
+            // 如果是新单词的话编码长度增加新单词的长度+1，否则不变。
+            return isNew? word.length() + 1: 0;
+        }
+    }
+    class TrieNode {
+        char val;
+        TrieNode[] children = new TrieNode[26];
+
+        public TrieNode() {}
+    }
+
+    /**
      * 3月27日 每日一题  914.卡牌分组
      * 思路：求各个数字出现次数的最大公约数
      * 题解：https://leetcode-cn.com/problems/x-of-a-kind-in-a-deck-of-cards/solution/3ms-jian-dan-java-fu-zeng-reducexie-fa-miao-dong-b/
