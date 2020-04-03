4f56f1a1f0bbfaaddce367b66db88fe018e76d83
mambo-wang <mambo1991@163.com>
2020-04-03 11:03:17 +0800

    WuKong--leetCode每日一题4.3 8 字符串转换整数

--- wukong-scripture/src/main/java/com/wukong/service/leetcode/daily/Daily202004.java
+++ wukong-scripture/src/main/java/com/wukong/service/leetcode/daily/Daily202004.java
@@ -1,9 +1,54 @@
 package com.wukong.service.leetcode.daily;
 
-public class Daily202004 {
+public class
+Daily202004 {
 
     public static void main(String[] args) {
 
+        Daily202004 daily202004 = new Daily202004();
+        System.out.println(daily202004.myAtoI("   -22342iutreit"));
+    }
+
+    /**
+     * leetcode每日一题 4月3日
+     * 8 字符串转换整数
+     *
+     * @return
+     */
+    public int myAtoI(String str){
+        char[] ca = str.toCharArray();
+        int n  = ca.length;
+        int idx = 0;
+
+        while (idx < n && ca[idx] == ' '){
+            //去掉前导空格
+            idx ++;
+        }
+        if(idx == n){
+            return 0;
+        }
+        boolean negative = false;
+        if(ca[idx] == '-'){
+            negative = true;//负数
+            idx ++;
+        } else if(ca[idx] == '+'){
+            idx ++;
+        } else if(!Character.isDigit(ca[idx])){
+            return 0;
+        }
+
+        int ans = 0;
+        while (idx < n && Character.isDigit(ca[idx])){
+
+            int digit = ca[idx] - '0';
+            if(ans > (Integer.MAX_VALUE - digit)/10){//防止越界
+                return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            ans = ans * 10 + digit;
+            idx ++;
+        }
+        return negative ? -ans : ans;
     }
 
 
