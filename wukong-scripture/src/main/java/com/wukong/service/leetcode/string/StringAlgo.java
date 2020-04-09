c0ec5f220bb250f9cdb8cdd704e6a8f17321dc22
mambo-wang <mambo1991@163.com>
2020-04-09 22:35:50 +0800

    WuKong--leetCode每日一题4.9 22 括号生成

--- wukong-scripture/src/main/java/com/wukong/service/leetcode/string/StringAlgo.java
+++ wukong-scripture/src/main/java/com/wukong/service/leetcode/string/StringAlgo.java
@@ -1,5 +1,8 @@
 package com.wukong.service.leetcode.string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringAlgo {
 
     public static void main(String[] args) {
@@ -12,17 +15,48 @@ public class StringAlgo {
 
     /**
      * 344. 反转字符串
+     *
      * @param s
      */
     public void reverseString(char[] s) {
-        int i = 0; int j=s.length -1;
+        int i = 0;
+        int j = s.length - 1;
 
-        while(i < j){
+        while (i < j) {
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
-            i ++;
-            j --;
+            i++;
+            j--;
         }
     }
+
+    /**
+     * leetCode每日一题 4月9日 22 括号生成
+     * 链接：https://leetcode-cn.com/problems/generate-parentheses/solution/jian-dan-dfsmiao-dong-by-sweetiee/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        dfs(n, n, "");
+        return res;
+    }
+
+    private void dfs(int left, int right, String curStr) {
+        if (left == 0 && right == 0) { // 左右括号都不剩余了，递归终止
+            res.add(curStr);
+            return;
+        }
+
+        if (left > 0) { // 如果左括号还剩余的话，可以拼接左括号
+            dfs(left - 1, right, curStr + "(");
+        }
+        if (right > left) { // 如果右括号剩余多于左括号剩余的话，可以拼接右括号
+            dfs(left, right - 1, curStr + ")");
+        }
+    }
+
+
 }
