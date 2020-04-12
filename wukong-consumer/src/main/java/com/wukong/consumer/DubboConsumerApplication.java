6841b3756941a3bbf9fb45eb706cc4e9d7be5424
mambo-wang <mambo1991@163.com>
2020-04-12 12:57:02 +0800

    WuKong--解决bug:dubbo消费者找不到服务：No provider available for the service

--- wukong-consumer/src/main/java/com/wukong/consumer/DubboConsumerApplication.java
+++ wukong-consumer/src/main/java/com/wukong/consumer/DubboConsumerApplication.java
@@ -1,9 +1,11 @@
 package com.wukong.consumer;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
