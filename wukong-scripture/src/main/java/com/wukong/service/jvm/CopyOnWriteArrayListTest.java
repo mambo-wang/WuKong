b8e88efa171cb6cc53177690ee22a2289ff8b63a
mambo-wang <mambo1991@163.com>
2020-06-27 11:05:03 +0800

    WuKong--CopyOnWriteArrayList实践，与ArrayList性能测试对比

---
+++ wukong-scripture/src/main/java/com/wukong/service/jvm/CopyOnWriteArrayListTest.java
@@ -0,0 +1,76 @@
+package com.wukong.service.jvm;
+
+import org.springframework.util.StopWatch;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 在 Java 中，CopyOnWriteArrayList 虽然是一个线程安全的 ArrayList，
+ * 但因为其实现方式是，每次修改数据时都会复制一份数据出来，
+ * 所以有明显的适用场景，即读多写少或者说希望无锁读的场景。
+ */
+public class CopyOnWriteArrayListTest {
+
+    public static void main(String[] args) {
+        CopyOnWriteArrayListTest copyOnWriteArrayListTest = new CopyOnWriteArrayListTest();
+        System.out.println(copyOnWriteArrayListTest.testRead());
+        System.out.println(copyOnWriteArrayListTest.testWrite());
+    }
+
+
+    //测试并发写的性能
+    public Map testWrite() {
+        List<Integer> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<>());
+        StopWatch stopWatch = new StopWatch();
+        int loopCount = 100000;
+        stopWatch.start("Write:copyOnWriteArrayList");
+        //循环100000次并发往CopyOnWriteArrayList写入随机元素
+        IntStream.rangeClosed(1, loopCount).parallel().forEach(__ -> copyOnWriteArrayList.add(ThreadLocalRandom.current().nextInt(loopCount)));
+        stopWatch.stop();
+        stopWatch.start("Write:synchronizedList");
+        //循环100000次并发往加锁的ArrayList写入随机元素
+        IntStream.rangeClosed(1, loopCount).parallel().forEach(__ -> synchronizedList.add(ThreadLocalRandom.current().nextInt(loopCount)));
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+        Map result = new HashMap();
+        result.put("copyOnWriteArrayList", copyOnWriteArrayList.size());
+        result.put("synchronizedList", synchronizedList.size());
+        return result;
+    }
+
+    //帮助方法用来填充List
+    private void addAll(List<Integer> list) {
+        list.addAll(IntStream.rangeClosed(1, 1000000).boxed().collect(Collectors.toList()));
+    }
+
+    //测试并发读的性能
+    public Map testRead() {
+        //创建两个测试对象
+        List<Integer> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<>());
+        //填充数据
+        addAll(copyOnWriteArrayList);
+        addAll(synchronizedList);
+        StopWatch stopWatch = new StopWatch();
+        int loopCount = 1000000;
+        int count = copyOnWriteArrayList.size();
+        stopWatch.start("Read:copyOnWriteArrayList");
+        //循环1000000次并发从CopyOnWriteArrayList随机查询元素
+        IntStream.rangeClosed(1, loopCount).parallel().forEach(__ -> copyOnWriteArrayList.get(ThreadLocalRandom.current().nextInt(count)));
+        stopWatch.stop();
+        stopWatch.start("Read:synchronizedList");
+        //循环1000000次并发从加锁的ArrayList随机查询元素
+        IntStream.range(0, loopCount).parallel().forEach(__ -> synchronizedList.get(ThreadLocalRandom.current().nextInt(count)));
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+        Map result = new HashMap();
+        result.put("copyOnWriteArrayList", copyOnWriteArrayList.size());
+        result.put("synchronizedList", synchronizedList.size());
+        return result;
+    }
+}
